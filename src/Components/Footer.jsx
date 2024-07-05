0de359d116504ede4a34bef343df75a6faa51a15
ShivendraSinghJadoun <shivendra.jadoun@gmail.com>
2024-07-05 15:56:11 +0530

    commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -12,33 +12,33 @@ const Footer = () => {
           <p className="text-lg font-semibold">Contact</p>
           <p className="py-1 flex items-center">
             <FontAwesomeIcon icon={faEnvelope} className="mr-2" />
-            <a href="mailto:kashyap1strishi@gmail.com" className="hover:text-gray-300">kashyap1strishi@gmail.com</a>
+            <a href="mailto:kashyap1strishi@gmail.com" className="hover:text-gray-300">Shivendra.jadoun@gmail.com</a>
           </p>
           <p className="py-1 flex items-center">
             <FontAwesomeIcon icon={faPhone} className="mr-2" />
-            <a href="tel:9878402898" className="hover:text-gray-300">9878402898</a>
+            <a href="tel:9953461619" className="hover:text-gray-300">9953461619</a>
           </p>
         </div>
         <div className="mb-4 md:mb-0">
           <p className="text-lg font-semibold">Location</p>
           <p className="py-1 flex items-center">
             <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-2" />
-            Rajiv Nagar, Sector 13
+            Sector 10 near civil hospital
           </p>
           <p className="py-1">Gurugram, Haryana, 122001, India</p>
         </div>
         <div className="flex space-x-6">
-          <a href="https://www.linkedin.com/in/rishi-kashyap" className="text-gray-100 hover:text-gray-300" aria-label="LinkedIn">
+          <a href="www.linkedin.com/in/shivendra-jadoun-4856882b9" className="text-gray-100 hover:text-gray-300" aria-label="LinkedIn">
             <FontAwesomeIcon icon={faLinkedin} size="2x" />
           </a>
-          <a href="https://github.com/rishi-kashyap" className="text-gray-100 hover:text-gray-300" aria-label="Github">
+          <a href="https://github.com/ShivendraSinghJadoun" className="text-gray-100 hover:text-gray-300" aria-label="Github">
             <FontAwesomeIcon icon={faGithub} size="2x" />
           </a>
         </div>
       </div>
       <hr className="my-4 border-gray-700" />
       <div className="text-center">
-        <p className="text-gray-400 text-sm">&copy; {new Date().getFullYear()} Developed by @kashyap</p>
+        <p className="text-gray-400 text-sm">&copy; {new Date().getFullYear()} Developed by @Shivendra</p>
       </div>
     </footer>
   );
