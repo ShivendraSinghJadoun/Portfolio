9c1e00b7f2b34e370746632d718bf273987373b5
ShivendraSinghJadoun <shivendra.jadoun@gmail.com>
2024-09-14 14:45:37 +0530

    add

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -16,7 +16,7 @@ const Profile = () => {
           className="rounded-full w-40 h-40 mx-auto mb-4 shadow-lg transform transition-transform duration-500 hover:scale-105 animate-zoom-in" 
         />
         <p className="text-lg md:text-xl text-gray-200 w-[90%] md:w-[70%] mx-auto animate-fade-in">
-          Passionate MERN Stack Developer with hands-on experience in building scalable web applications using MongoDB, Express.js, React.js, and Node.js. Proficient in crafting efficient backend architectures, implementing RESTful APIs, and designing responsive user interfaces. Skilled in version control with Git and deployment with platforms like Heroku. Committed to staying updated with the latest technologies and contributing to cutting-edge projects.
+          Passionate MERN Stack Developer with hands-on experience in building scalable web applications using MongoDB, Express.js, React.js, and Node.js. Proficient in crafting efficient backend architectures, implementing RESTful APIs, and designing responsive user interfaces. Skilled in version control with Git and deployment with platforms like vercel and render. Committed to staying updated with the latest technologies and contributing to cutting-edge projects.
         </p>
       </div>
       <a href={MERN} download target='_blank' className="bg-blue-600 text-white text-xl font-medium py-2 px-6 rounded-lg shadow-md hover:bg-blue-700 transition-all duration-300 transform hover:scale-105">
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,9 +1,9 @@
 
 
 import React from "react";
-import Learn from "../assests/Learnit.png";
-import billing from "../assests/billing.png";
-import image from "../assests/image.png";
+import Learn from "../assests/passop.png";
+// import billing from "../assests/billing.png";
+import image from "../assests/foody zone.png";
 
 function Projects() {
   return (
@@ -19,9 +19,10 @@ function Projects() {
           <ul className="list-disc pl-5 text-lg space-y-2 animate-fade-in">
             <li>In this project you  Add Name, Add password, Delete password, And Copy everything </li>
             <li>The user dashboard allows  to view and update their details and track everything.</li>
+            <li>Its uses React Hooks for StateManagement and side effects and localstorage for persistent data storage.</li>
           </ul>
           <div className="btns flex flex-col md:flex-row gap-4 justify-center items-center mt-4">
-            <a href="https://learniitt.netlify.app/" target="_blank">
+            <a href="" target="_blank">
               <button className="bg-blue-600 text-white text-xl font-medium py-2 px-6 rounded-lg shadow-md hover:bg-blue-700 transition-all duration-300 transform hover:scale-105">View Project</button>
             </a>
             <a href="" target="_blank">
@@ -35,17 +36,17 @@ function Projects() {
 
       <div className="flex flex-col md:flex-row justify-center items-center gap-8 p-4 w-full md:w-3/4 border-b-2  rounded-lg animate-slide-up">
         <div className="w-full md:w-1/2 transform transition-transform duration-500 hover:scale-105">
-          <img src={image} alt="E-commerce Platform" className="w-full h-auto rounded-lg shadow-md animate-zoom-in" />
+          <img src={image} alt="Foody zone" className="w-full h-auto rounded-lg shadow-md animate-zoom-in" />
         </div>
         <div className="w-full md:w-1/2 flex flex-col gap-4">
-          <h3 className="text-2xl font-bold animate-fade-in">is..store</h3>
-          <p className="text-lg animate-fade-in">Developed a feature-rich e-commerce platform using React and Tailwind CSS.</p>
+          <h3 className="text-2xl font-bold animate-fade-in">Foody Store</h3>
+          <p className="text-lg animate-fade-in">Developed a feature foody zone platform using React and Figma CSS.</p>
           <ul className="list-disc pl-5 text-lg space-y-2 animate-fade-in">
-            <li>User registration and login functionalities to manage user accounts securely.</li>
-            <li>Browse and search for products with detailed descriptions and pricing information.</li>
-            <li>Shopping cart functionality to add and remove products, with real-time updates.</li>
-            <li>Secure checkout process with integrated payment gateways for seamless transactions.</li>
-            <li>Admin dashboard for managing products, categories, and orders efficiently.</li>
+            <li>Foody Zone is a responsive web app designed to offer users a seamless food browsing experience.</li>
+            <li>Using React.js for dynamic component-based architecture, CSS for styling, and Figma for wireframing and prototyping.</li>
+            <li>this app provides a user-friendly interface with a modern design.</li>
+            <li>UI incorporates a simple search functionality and category-based food filters like Breakfast, Lunch, and Dinner.</li>
+            
           </ul>
           <div className="btns flex flex-col md:flex-row gap-4 justify-center items-center mt-4">
             <button className="bg-blue-600 text-white text-xl font-medium py-2 px-6 rounded-lg shadow-md hover:bg-blue-700 transition-all duration-300 transform hover:scale-105">View Project</button>
