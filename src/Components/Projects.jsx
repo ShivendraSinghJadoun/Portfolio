248768db7f088b49504ff9f23ecce723366154bf
ShivendraSinghJadoun <shivendra.jadoun@gmail.com>
2024-09-16 11:51:02 +0530

    add something

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -22,7 +22,7 @@ function Projects() {
             <li>Its uses React Hooks for StateManagement and side effects and localstorage for persistent data storage.</li>
           </ul>
           <div className="btns flex flex-col md:flex-row gap-4 justify-center items-center mt-4">
-            <a href="" target="_blank">
+            <a href="https://passop-one.vercel.app" target="_blank">
               <button className="bg-blue-600 text-white text-xl font-medium py-2 px-6 rounded-lg shadow-md hover:bg-blue-700 transition-all duration-300 transform hover:scale-105">View Project</button>
             </a>
             <a href="" target="_blank">
